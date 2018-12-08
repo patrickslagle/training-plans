4cf01567398fa3b4438a6df9f935c498e3fba752
Patrick Slagle <slaglebagel17@gmail.com>
2018-12-07 21:27:18 -0800

    added additional styling

--- client/index.js
+++ client/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
+import Home from './components/home.jsx';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-
+ReactDOM.render(<Home />, document.getElementById('root'));
