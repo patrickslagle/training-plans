a72f7d96b7ae50564e289390dc9d47c470f4eb3e
Patrick Slagle <slaglebagel17@gmail.com>
2018-12-07 20:56:18 -0800

    added functionality so subcategories appear when hovered over

--- client/components/workout-category.jsx
+++ client/components/workout-category.jsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../styles/workout-categories.css';
 
-const WorkoutCategory = (props) => {
+const WorkoutCategory = ({ category, subcategories, img, loadWorkouts, count }) => {
+  const [showSubCategories, toggleSubcategories] = useState(false)
+
+  function showCategoryInfo() {
+    // if mouse is over category, show subcategories
+    if (showSubCategories) {
+      return subcategories.map(subcategory => (
+        <button
+          key={subcategory}
+          type="button"
+          className="workout-subcategory"
+        >
+          {subcategory}
+        </button>
+      ));
+    }
+    // otherwise, show category title
+    return <h1 className="workout-category-title">{category}</h1>;
+  }
+
   return (
-    <div></div>
+    <div
+      className="workout-category"
+      onMouseEnter={() => toggleSubcategories(true)}
+      onMouseLeave={() => toggleSubcategories(false)}
+    >
+      {showCategoryInfo()}
+    </div>
   )
 };
 
