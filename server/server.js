c654a7df3118d54193981616696750e0df2c3136
Patrick Slagle <slaglebagel17@gmail.com>
2018-12-07 22:15:00 -0800

    imported and using a router

--- server/server.js
+++ server/server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const path = require('path');
+const router = require('./router');
 
 const app = express();
-
 const filePath = path.resolve(__dirname, '../public');
 app.use(express.static(filePath));
 
-app.listen(3001, () => console.log('Listening on PORT: 3000'));
+// route different requests
+router(app);
+
+// handle any errors
+app.use((err, req, res, next) => {
+  res.json({ error: err.message });
+});
+
+app.listen(3000, () => console.log('Listening on PORT: 3000'));
