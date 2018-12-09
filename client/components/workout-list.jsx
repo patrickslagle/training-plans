61d1c69d8c0ccd8184b81b6e6183e76afe8000a6
Patrick Slagle <slaglebagel17@gmail.com>
2018-12-08 16:19:26 -0800

    added functionality to workout-list to render a list of workouts

--- client/components/workout-list.jsx
+++ client/components/workout-list.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import WorkoutItem from './workout-item.jsx';
 
-const WorkoutList = (props) => {
+const WorkoutList = ({ workouts, changeApp }) => {
+  const [workoutItems, updateWorkouts] = useState([]);
+  useEffect(() => {
+    updateWorkouts(workouts.map(workout => (
+      <WorkoutItem
+        key={workout._source.sku}
+        workout={workout._source}
+        changeApp={changeApp}
+      />
+    )));
+  });
+
   return (
-    <div></div>
-  )
+    <div>
+      {workouts.length && <h2>Select Workout</h2>}
+      <div id="workout-plans">
+        {workoutItems}
+      </div>
+    </div>
+  );
 };
 
 export default WorkoutList;
