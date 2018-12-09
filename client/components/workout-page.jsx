66fdfb1583347e54e8fc5784e7dcb5eefaf01c33
Patrick Slagle <slaglebagel17@gmail.com>
2018-12-09 12:24:11 -0800

    added props to workout page and basic rendering

--- client/components/workout-page.jsx
+++ client/components/workout-page.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+// look to import https://github.com/cure53/DOMPurify
+// or lodash escape https://lodash.com/docs#escape
 
-const WorkoutPage = (props) => {
+const WorkoutPage = ({workout}) => {
+  const {
+    title,
+    icon,
+    description,
+    price,
+    workoutCount,
+    weeksDuration,
+    authorFullName,
+  } = workout;
   return (
-    <div></div>
+    <div>
+      <p>{title}</p>
+      <p>{icon}</p>
+      <p>{price}</p>
+      <p>{workoutCount}</p>
+      <p>{weeksDuration}</p>
+      <p>{authorFullName}</p>
+      <div dangerouslySetInnerHTML={{__html: description }} />
+    </div>
   )
 };
 
