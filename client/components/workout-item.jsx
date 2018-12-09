d2645928874400007f5d31600e6cdc9302f8c81e
Patrick Slagle <slaglebagel17@gmail.com>
2018-12-08 16:19:48 -0800

    added functionality to render a specific workout item

--- client/components/workout-item.jsx
+++ client/components/workout-item.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import WorkoutPage from './workout-page.jsx';
 
-const WorkoutItem = (props) => {
+const WorkoutItem = ({ workout, changeApp }) => {
+  const { icon, title, weeksDuration, price } = workout;
   return (
-    <div></div>
+    <button
+      className="workout-item"
+      onClick={() => changeApp(<WorkoutPage workout={workout} />)}
+      type="button"
+    >
+      <img src={icon} alt="workout type" />
+      <p>{title}</p>
+      <p>{weeksDuration}</p>
+      <p>{price}</p>
+    </button>
   )
 };
 
