055d8a2c626488891c196db6e6feea5952623877
Patrick Slagle <slaglebagel17@gmail.com>
2018-12-09 22:08:57 -0800

    removed unused code

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import Adapter from 'enzyme-adapter-react-16';
-import { shallow, configure } from 'enzyme';
 import renderer from 'react-test-renderer';
 import App from '../client/components/app.jsx';
 import Home from '../client/components/home.jsx';
 import WorkoutCategory from '../client/components/workout-category.jsx';
 import WorkoutItem from '../client/components/workout-item.jsx';
 import WorkoutList from '../client/components/workout-list.jsx';
-import WorkoutPage from '../client/components/workout-page.jsx';
-
-
-configure({ adapter: new Adapter() });
 
 test('App component renders properly', () => {
   const component = renderer.create(
